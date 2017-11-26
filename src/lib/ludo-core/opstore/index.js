346ed68f04f43e375a03ab46f44d58e158fab74d
Marius Lundgård <studio@mariuslundgard.com>
2017-11-26 19:07:30 +0100

    Annotate `opstore` implementation

--- src/lib/ludo-core/opstore/index.js
+++ src/lib/ludo-core/opstore/index.js
@@ -7,6 +7,7 @@ import type {IStore} from 'opstore'
 import type {BrowserContext, Props, State} from '../types'
 
 export function mount (elm: any, props: Props): BrowserContext {
+  // Create store with initial state
   const store: IStore<State> = createStore({
     playing: false,
     time: elm.currentTime || 0,
@@ -14,8 +15,10 @@ export function mount (elm: any, props: Props): BrowserContext {
     bufferRanges: getBufferRanges(elm)
   })
 
+  // Create a store reference on which to perform operations
   const ref = store.ref()
 
+  // Create controls that can be used in views
   const controls = {
     play: () => elm.play(),
     pause: () => elm.pause(),
@@ -23,6 +26,7 @@ export function mount (elm: any, props: Props): BrowserContext {
     setPosition: position => (elm.currentTime = elm.duration * position)
   }
 
+  // Create event handler functions
   const handlePlay = () => ref.set('playing', true)
   const handlePause = () => ref.set('playing', false)
   const handleTimeupdate = () => ref.set('time', elm.currentTime)
@@ -30,23 +34,23 @@ export function mount (elm: any, props: Props): BrowserContext {
   const handleProgress = () => ref.set('bufferRanges', getBufferRanges(elm))
 
   function unmount () {
+    // Unlisten to events
     elm.removeEventListener('play', handlePlay)
     elm.removeEventListener('pause', handlePause)
     elm.removeEventListener('timeupdate', handleTimeupdate)
     elm.removeEventListener('durationchange', handleDurationchange)
     elm.removeEventListener('progress', handleProgress)
-
     if (props.onClick) {
       elm.removeEventListener('click', props.onClick)
     }
   }
 
+  // Listen to events
   elm.addEventListener('play', handlePlay)
   elm.addEventListener('pause', handlePause)
   elm.addEventListener('timeupdate', handleTimeupdate)
   elm.addEventListener('durationchange', handleDurationchange)
   elm.addEventListener('progress', handleProgress)
-
   if (props.onClick) {
     elm.addEventListener('click', props.onClick)
   }
